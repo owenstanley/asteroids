384d146e8f40050c2bf9cbc86f9da6712e643f6f
Owen Stanley <owentgstanley@gmail.com>
2024-11-11 18:45:41 +0000

    adding Asteroid and AsteroidField classes, updating gameloop with groups

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # initialising pygame
@@ -8,7 +10,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -16,9 +27,12 @@ def main():
                 return
 
         screen.fill("#000000")
-        player.draw(screen)
 
-        player.update(dt)
+        for player in updatable:
+            player.update(dt)
+
+        for player in drawable:
+            player.draw(screen)
 
         pygame.display.flip()
 
