4b8604bb68f2e7ceb5c6771ecd9365060c4b7ba9
Owen Stanley <owentgstanley@gmail.com>
2024-11-11 16:59:04 +0000

    creating base circleshape class

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pygame
 from constants import *
 
 def main():
+    # initialising pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +15,9 @@ def main():
 
         screen.fill("#000000")
         pygame.display.flip()
+
+        # limit framerate to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
